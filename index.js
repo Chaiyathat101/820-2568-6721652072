f30081f7aa33ae0c6cef750bf6ca813cf07423f3
Chaiyathat101 <chaiyathat.t@ku.th>
2026-02-16 16:46:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
-// ทำการ import http
-const http = require('http');
-const host = 'localhost';
-const port = 8000;
+const express = express();
 
-// กำหนดค่าเริ่มต้นของเซิร์ฟเวอร์เมื่่อเปิดใช้งานเว็บผ่านเบราว์เซอร์ ที่ localhost:8000
+const app = express();
 
-const requireListener = function(req, res) {{
-    res.writeHead(200);
-    res.end('My 1st server\n');
+const port = 8000();
+
+const = (body-paster.text());
+
+let users = []
+
+// path - /
+app.get('/', (req) => )
+resizeBy.sent ('hello world');
+let user = {
+    name : "john doe"
+    age : "30"
+    email : "john.12345@gmail.com"
+}
+res.json(user)
+
+// path = post /user 
+app.post ("/user", Request =>)
+let user req = res body; 
+user id = counter 
+counter += 1 
+res.send(req.body);
+user.push(user);
+
+res.json {
+res : "User added suscufuly"
+let id = req.pareas.id
+res.send(id)
+}; 
+
+app.listen(port8000) . => {
+    console.log("Sever is runing on port $(port)")
+
+}; 
+
+let selectdindex = user.findindex(user => )
+if (user.id -- id) {
+    return true 
+} else {
+    return false
 }
 
-// run server
-const server = http.createServer(requireListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+res.send(selectdindex + '')
+message: 
+data : {
+    user: updateUser,
+    indexUpdate: selectdindex
+
+    users[selectdindex].name = updateUser name ||  users.name
+    users[selectdindex].age = updateUser age ||  users.age
+
+    let id = req.pareas.id
+    let selectdindex = user findIndex(user -> user.id -- id )
+
+    req json(((
+        message : "User deled susecfuily"
+        data: {
+            indexDeled.setInterval(() => { 
+                let deled user.id || user.name
+                message : 'User not fund'
+            }, interval);
+        }
+    }
+        )
+
+}
+))
